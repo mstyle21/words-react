16dbbf0b449bf1ef510ec43a3df2283b2a28ad20
mStyle21 <mr.r.mihai@gmail.com>
2023-11-01 21:52:02 +0200

    new stages added

--- src/data/stages.ts
+++ src/data/stages.ts
@@ -11,4 +11,12 @@ export const stages: TStageDetails[] = [
   { level: 5, words: ["ventil", "nivel", "linte", "inel", "vin", "tei"] },
   { level: 6, words: ["acord", "doar", "caro", "dar", "arc"] },
   { level: 7, words: ["școală", "oală", "șoc", "loc", "laș", "coș"] },
+  { level: 8, words: ["concret", "concert", "corect", "conte", "corn", "cont", "cerc"] },
+  { level: 9, words: ["chitară", "chiar", "iaht", "iar", "arc", "act"] },
+  { level: 10, words: ["bucătar", "trabuc", "curat", "cură", "acru", "abur"] },
+  { level: 11, words: ["vreodată", "otravă", "roată", "odată", "doar", "dată"] },
+  { level: 12, words: ["decolare", "dolar", "acord", "deal", "clar", "cald"] },
+  { level: 13, words: ["recoltă", "cretă", "rocă", "toc", "rol", "cer"] },
+  { level: 14, words: ["scândură", "urnă", "rând", "dună", "când", "curs", "râs"] },
+  { level: 15, words: ["cerneală", "ceară", "carne", "arenă", "elan", "cană"] },
 ];
